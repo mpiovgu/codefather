2924f70a5e336331f030c3d7f1aaf565654bbebd
mpiovgu <39343862+mpiovgu@users.noreply.github.com>
2026-06-12 18:10:38 +0800

    fix: 简化config.js plugin配置，移除有问题的SEO和Sitemap插件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -40,7 +40,7 @@ module.exports = {
   permalink: "/:slug",
 
   // 监听文件变化，热更新
-  extraWatchFiles: [".vuepress/*.ts", ".vuepress/sidebars/*.ts"],
+  extraWatchFiles: [".vuepress/*.js", ".vuepress/sidebars/*.js"],
   markdown: {
     // 开启代码块的行号
     lineNumbers: true,
@@ -48,68 +48,10 @@ module.exports = {
     extractHeaders: ["h2", "h3", "h4", "h5", "h6"],
   },
   plugins: [
-    ["@vuepress/back-to-top"],
-    // Google 分析
-    [
-      "@vuepress/google-analytics",
-      {
-        ga: "GTM-WVS9HM6W",
-      },
-    ],
+    "@vuepress/back-to-top",
     ["@vuepress/medium-zoom"],
-    // https://github.com/lorisleiva/vuepress-plugin-seo
-    [
-      "seo",
-      {
-        siteTitle: (_, $site) => $site.title,
-        title: ($page) => $page.title,
-        description: ($page) =>
-          $page.frontmatter.description || $page.description,
-        author: (_, $site) => $site.themeConfig.author || author,
-        tags: ($page) => $page.frontmatter.tags || tags,
-        type: ($page) => "article",
-        url: (_, $site, path) =>
-          ($site.themeConfig.domain || domain || "") + path,
-        image: ($page, $site) =>
-          $page.frontmatter.image &&
-          (($site.themeConfig.domain &&
-            !$page.frontmatter.image.startsWith("http")) ||
-            "") + $page.frontmatter.image,
-        publishedAt: ($page) =>
-          $page.frontmatter.date && new Date($page.frontmatter.date),
-        modifiedAt: ($page) => $page.lastUpdated && new Date($page.lastUpdated),
-      },
-    ],
-    // https://github.com/ekoeryanto/vuepress-plugin-sitemap
-    [
-      "sitemap",
-      {
-        hostname: domain,
-      },
-    ],
-    // https://github.com/IOriens/vuepress-plugin-baidu-autopush
-    ["vuepress-plugin-baidu-autopush"],
-    // https://github.com/zq99299/vuepress-plugin/tree/master/vuepress-plugin-tags
-    ["vuepress-plugin-tags"],
-    // https://github.com/znicholasbrown/vuepress-plugin-code-copy
-    [
-      "vuepress-plugin-code-copy",
-      {
-        successText: "代码已复制",
-      },
-    ],
-    // https://github.com/webmasterish/vuepress-plugin-feed
-    [
-      "feed",
-      {
-        canonical_base: domain,
-        count: 10000,
-        // 需要自动推送的文档目录
-        posts_directories: [],
-      },
-    ],
-    // https://github.com/tolking/vuepress-plugin-img-lazy
-    ["img-lazy"],
+    ["vuepress-plugin-code-copy", { successText: "代码已复制" }],
+    "vuepress-plugin-img-lazy",
   ],
   // 主题配置
   themeConfig: {
